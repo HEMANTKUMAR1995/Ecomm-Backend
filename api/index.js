9ef036497b8e5a09cc1c19fe7eec4cd9d7eed8b2
S.HEMANT KUMAR <sunny.hemant.kumar96@gmail.com>
2025-03-31 12:58:15 +0530

    chagne from server to index

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoutes.js";
-import cartRouter from "./routes/cartRoute.js";
+import { connectDB } from "../config/db.js";
+import foodRouter from "../routes/foodRoute.js";
+import userRouter from "../routes/userRoutes.js";
+import cartRouter from "../routes/cartRoute.js";
 import "dotenv/config.js";
 
 const app = express();
